2d920acc968345591d8c6b6ab89a4000bc3ebd71
shekit <as7790@nyu.edu>
2015-09-10 13:25:48 -0400

    added helper text to arrow image

--- client/js/client.js
+++ client/js/client.js
@@ -302,21 +302,6 @@ Template.body.onRendered(function(){
 	checkForStyles(fontStyleLength);
 })
 
-Template.ga.onRendered(function(){
-	new GA('UA-67448502-1');
-})
-
-GA = function(code){
-	var _gaq = window._gaq || [];
-    _gaq.push(['_setAccount', code]);
-    _gaq.push(['_trackPageview']);
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-}
-
 // EVENTS
 Template.body.events({
 	"keydown": function(event){
@@ -625,5 +610,9 @@ Template.downloadFont.helpers({
 	"link" : function(){
 		return fontSwitchCase("link")
 	}
+})
+
+Router.route('/', function(){
+	this.render('main');
 })
 
